70ccb167edd4eae0d32539767239638473b2dfa7
hjdisley <hjdisley@gmail.com>
2022-05-19 09:54:02 +0100

    chunk and anagrams completed

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // Clean 1st and second string and split the first and second strings into an array of characters
+  const first = stringA.replace(/[^\w]/g, '').split('');
+  const second = stringB.replace(/[^\w]/g, '').split('');
+
+  // Initialise a variable that will be used to check the current letter of stringA against stringB
+  // Start it with the first letter in stringA
+  let currentLetter = first[0];
+
+  // Loop through the first string
+  for (let i = 0; i < first.length; i++) {
+    // If the second string doesnt include the first string then return false
+    if (!second.includes(currentLetter)) {
+      return false;
+    } else {
+      // Otherwise if the current letter is the last letter of the first array return true
+      if (currentLetter == first[first.length - 1]) {
+        return true;
+      } else {
+        // But if the second string still has letters that havent been checked return false
+        if (second.length > first.length) {
+          return false;
+        } else {
+          // Otherwise current letter checked becomes the next letter in the array
+          currentLetter = first[i + 1];
+        }
+      }
+    }
+  }
+}
+
+/* 
+ANAGRAMS SOLUTION 2:
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+*/
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,17 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //Initialise the final array
+  const finalArr = [];
+  // Loop through the array and increment by size
+  for (let i = 0; i < array.length; i += size) {
+    // Slice from the current index size times
+    const arrayChunk = array.slice(i, i + size);
+    // Push the chunk into the finalArr
+    finalArr.push(arrayChunk);
+  }
+  return finalArr;
+}
 
 module.exports = chunk;
